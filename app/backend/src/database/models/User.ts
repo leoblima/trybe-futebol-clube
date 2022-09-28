ae9834a4a3d3047235ad0a56e65e1c059463560e
Leonardo Lima <leoblima00@gmail.com>
2022-09-28 15:55:10 -0300

    Feat: change name of the seeders to be reconize and start to create migration and model to user

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,41 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class UserModel extends Model {
+  public id: number;
+  public username!: string;
+  public role!: string;
+  public email!: string;
+  public password!: string;
+}
+
+UserModel.init({
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  username: {
+    type: new DataTypes.STRING(255),
+    allowNull: false
+  },
+  role: {
+    type: new DataTypes.STRING(255),
+    allowNull: false
+  },
+  email: {
+    type: new DataTypes.STRING(255),
+    allowNull: false
+  },
+  password: {
+    type: new DataTypes.STRING(255),
+    allowNull: false
+  }
+}, {
+  sequelize: db,
+  modelName: 'UserModel',
+  timestamps: false,
+  underscored: true,
+});
+
+export default UserModel;
--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -4,7 +4,9 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
+import UserModel from '../database/models/User';
+
+import mockFindAllUser from './mocks/userModelFindAll';
 
 import { Response } from 'superagent';
 
@@ -12,32 +14,25 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+describe('Testa Models e Migrations para Users tab', () => {
+  let chaiHttpResponse: Response;
 
-  // let chaiHttpResponse: Response;
-
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
-
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
-
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
+    before(async () => {
+      sinon
+        .stub(UserModel, "findAll")
+        .resolves([
+          ...mockFindAllUser
+        ]);
+    }) 
+    after(()=>{
+      (Example.findOne as sinon.SinonStub).restore();
+    } 
+    it('...', async () => {
+      chaiHttpResponse = await chai
+          .request(app)
+          .. 
+      expect(...)
+    });
 
   it('Seu sub-teste', () => {
     expect(false).to.be.eq(true);
